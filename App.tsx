7edfff2fa6446dbaadcc23438ec5e4a0a6a33ce6
Sebastian Perez <sperezm97@gmail.com>
2021-02-02 21:06:27 -0400

    Add pre commit for linters

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Preuba si funciona</Text>
+    </View>
+  );
+};
+
+export default App;
