e851c6f2f58bd3b20189c74a80db78c484caa76e
Sebastian Perez <sperezm97@gmail.com>
2021-02-02 20:49:51 -0400

    add path linters

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@store': './src/store',
+          '@config': './src/config',
+          '@assets': './src/assets',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+  ],
 };
